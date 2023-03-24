96b50094409217d42fb16bd338ab03f843df269f
skiflok <anisimov.a.v@inbox.ru>
2023-03-24 08:44:51 +0300

    test

--- src/main/java/ConsoleHelper.java
+++ src/main/java/ConsoleHelper.java
@@ -1,2 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class ConsoleHelper {
+
+    private static final Logger logger = Logger.getLogger(ConsoleHelper.class.getName());
+
+    static private final BufferedReader bis =
+            new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() {
+        while (true) {
+            try {
+                String buf = bis.readLine();
+                if (buf != null)
+                    return buf;
+            } catch (IOException e) {
+                logger.log(Level.SEVERE, "Произошла ошибка при попытке ввода текста.");
+                writeMessage("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+            }
+        }
+    }
+
+    public static int readInt() {
+        while (true) {
+            try {
+                return Integer.parseInt(readString().trim());
+            } catch (NumberFormatException e) {
+                logger.log(Level.SEVERE, "Произошла ошибка при попытке ввода текста {0}.", e.toString());
+                writeMessage("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+            }
+        }
+    }
+
+
 }
