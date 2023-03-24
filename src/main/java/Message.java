f0d9ce617be386e678e4b7f93753d9441f522fff
skiflok <anisimov.a.v@inbox.ru>
2023-03-24 09:28:55 +0300

    Message

--- src/main/java/Message.java
+++ src/main/java/Message.java
@@ -1,2 +1,26 @@
-public class Message {
+import java.io.Serializable;
+
+
+public class Message implements Serializable {
+
+    private final MessageType messageType;
+    private final String message;
+
+    public Message(MessageType messageType) {
+        this.messageType = messageType;
+        message = null;
+    }
+
+    public Message(MessageType messageType, String message) {
+        this.messageType = messageType;
+        this.message = message;
+    }
+
+    public MessageType getMessageType() {
+        return messageType;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
