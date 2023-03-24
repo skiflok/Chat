8f07e056d4ed638f654d65c29a8ca71c06a6f975
skiflok <anisimov.a.v@inbox.ru>
2023-03-24 11:57:23 +0300

    serverHandshake

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,8 +1,17 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Server {
+
+    private static final Logger logger = Logger.getLogger(Server.class.getName());
+
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
+
     public static void main(String[] args) {
 
         ConsoleHelper.writeMessage("Введите порт сервера:");
@@ -11,14 +20,27 @@ public class Server {
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             ConsoleHelper.writeMessage("Чат сервер запущен.");
             while (true) {
-            Socket socket = serverSocket.accept();
-            new Handler(socket).start();
+                Socket socket = serverSocket.accept();
+                new Handler(socket).start();
             }
         } catch (Exception e) {
+            logger.log(Level.SEVERE, "Произошла ошибка при запуске или работе сервера {0}", e.getMessage());
             ConsoleHelper.writeMessage("Произошла ошибка при запуске или работе сервера.");
         }
 
 
+    }
+
+    public static void sendBroadcastMessage(Message message) {
+        for (Connection connection : connectionMap.values()) {
+            try {
+                connection.send(message);
+            } catch (IOException e) {
+                logger.log(Level.SEVERE, "Не смогли отправить сообщение {0}", e.getMessage());
+                logger.log(Level.SEVERE, "RemoteSocketAddress {0}", connection.getRemoteSocketAddress());
+                ConsoleHelper.writeMessage("Не смогли отправить сообщение " + connection.getRemoteSocketAddress());
+            }
+        }
     }
 
     private static class Handler extends Thread {
@@ -27,6 +49,21 @@ public class Server {
         public Handler(Socket socket) {
             this.socket = socket;
         }
+
+        private String serverHandshake(Connection connection) throws IOException, ClassNotFoundException {
+
+            connection.send(new Message(MessageType.NAME_REQUEST));
+
+            Message message = connection.receive();
+
+            connectionMap.put(message.getMessage(), connection);
+
+            return "";
+        }
+
+
+
+
     }
 
 }
