fb68700fecb6ff20731f4e084bfa269ce1888210
skiflok <anisimov.a.v@inbox.ru>
2023-03-27 10:48:20 +0300

    metod

--- src/main/java/app/client/Client.java
+++ src/main/java/app/client/Client.java
@@ -1,14 +1,53 @@
 package app.client;
 
 
-import app.Connection;
+import app.*;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Client {
+
+    private static final Logger logger = Logger.getLogger(Client.class.getName());
 
     protected Connection connection;
 
     private volatile boolean clientConnected;
 
+
+    protected String getServerAddress() {
+        ConsoleHelper.writeMessage("Введите адрес сервера:");
+        return ConsoleHelper.readString();
+    }
+
+    protected int getServerPort() {
+        ConsoleHelper.writeMessage("Введите порт сервера:");
+        return ConsoleHelper.readInt();
+    }
+
+    protected String getUserName() {
+        ConsoleHelper.writeMessage("Введите ваше имя:");
+        return ConsoleHelper.readString();
+    }
+
+    protected boolean shouldSentTextFromConsole() {
+        return true;
+    }
+
+    SocketThread getSocketThread() {
+        return new SocketThread();
+    }
+
+    void sendTextMessage(String text) {
+        try {
+            connection.send(new Message(MessageType.TEXT, text));
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Не удалось отправить сообщение");
+            clientConnected = false;
+            ConsoleHelper.writeMessage("Не удалось отправить сообщение");
+        }
+    }
 
 
     public class SocketThread extends Thread {
