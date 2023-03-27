035294336374897fbec3033a7c89f00557071c0a
skiflok <anisimov.a.v@inbox.ru>
2023-03-27 10:29:06 +0300

    Client

---
+++ src/main/java/app/client/Client.java
@@ -0,0 +1,20 @@
+package app.client;
+
+
+import app.Connection;
+
+public class Client {
+
+    protected Connection connection;
+
+    private volatile boolean clientConnected;
+
+
+
+    public class SocketThread extends Thread {
+
+
+    }
+
+
+}
