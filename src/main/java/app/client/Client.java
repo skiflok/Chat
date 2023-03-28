a0a59d206d937e333fd167cca37f811f666aee8d
skiflok <anisimov.a.v@inbox.ru>
2023-03-28 13:02:23 +0300

    test

--- src/main/java/app/client/Client.java
+++ src/main/java/app/client/Client.java
@@ -4,6 +4,7 @@ package app.client;
 import app.*;
 
 import java.io.IOException;
+import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -50,10 +51,41 @@ public class Client {
     }
 
 
+    public void run() {
+        SocketThread socketThread = getSocketThread();
+        socketThread.setDaemon(true);
+
+        socketThread.start();
+
+        try {
+            synchronized (this) {
+                wait();
+            }
+        } catch (InterruptedException e) {
+            ConsoleHelper.writeMessage("Произошла ошибка во время работы клиента.");
+            return;
+        }
+
+
+    }
+
+
     public class SocketThread extends Thread {
 
 
     }
 
+    public static void main(String[] args) throws IOException {
+        Client client = new Client();
+//        client.run();
+        Socket socket = new Socket("127.0.0.1", 8081);
+        client.connection = new Connection(socket);
+        String message;
+        while (true) {
+            message = ConsoleHelper.readString();
+            client.connection.send(new Message(MessageType.TEXT, message));
+        }
+
+    }
 
 }
