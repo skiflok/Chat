7e94f11bea36a8bfbcb077a0c1bb941165f2e94e
violator <violator@student.21-school.ru>
2023-07-03 16:52:07 +0300

    add contains

--- Server/src/main/java/com/example/dao/ActiveConnectionStorage.java
+++ Server/src/main/java/com/example/dao/ActiveConnectionStorage.java
@@ -1,52 +1,53 @@
 package com.example.dao;
 
 import io.netty.channel.Channel;
+import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 
+@Component
 public class ActiveConnectionStorage {
 
-    private static ActiveConnectionStorage instance = null;
+  private static ActiveConnectionStorage instance = null;
 
-    private static final Logger logger = LoggerFactory.getLogger(ActiveConnectionStorage.class);
+  private static final Logger logger = LoggerFactory.getLogger(ActiveConnectionStorage.class);
 
-    private final Map<String, Channel> connectionMap = new ConcurrentHashMap<>();
+  private final Map<String, Channel> connectionMap = new ConcurrentHashMap<>();
 
-    public Map<String, Channel> getConnectionMap() {
-        return connectionMap;
+  public Map<String, Channel> getConnectionMap() {
+    return connectionMap;
+  }
+
+    public Collection<Channel> getConnectionList () {
+        return connectionMap.values();
     }
 
-    private ActiveConnectionStorage() {
+  private ActiveConnectionStorage() {
+  }
+
+    public boolean contains(String userName) {
+        return connectionMap.containsKey(userName);
     }
 
-
-    public static ActiveConnectionStorage getInstance() {
-        if (instance == null) {
-            instance = new ActiveConnectionStorage();
-        }
-        return instance;
+  public static ActiveConnectionStorage getInstance() {
+    if (instance == null) {
+      instance = new ActiveConnectionStorage();
     }
+    return instance;
+  }
 
-    public void addUser (String userName, Channel channel) {
-        //TODO
-        connectionMap.put(userName, channel);
-    }
+  public void addUser(String userName, Channel channel) {
+    //TODO
+    connectionMap.put(userName, channel);
+  }
 
-    public void removeUser (String userName) {
-        //TODO
-        connectionMap.remove(userName);
-    }
+  public void removeUser(String userName) {
+    //TODO
+    connectionMap.remove(userName);
+  }
 
-    @Override
-    public String toString() {
-        StringBuilder res = new StringBuilder();
-        for (var entry: connectionMap.entrySet()) {
-            res.append(entry);
-            res.append("\n");
-        }
-        return res.toString();
-    }
 }
