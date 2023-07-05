918641087cdc04368a90607112ca08c3e52a8dbd
violator <violator@student.21-school.ru>
2023-07-05 14:48:06 +0300

    RoomRepository

---
+++ Server/src/main/java/com/example/repositories/roomRepositories/RoomRepository.java
@@ -0,0 +1,8 @@
+package com.example.repositories.roomRepositories;
+
+import com.example.model.Room;
+import com.example.repositories.CrudRepository;
+
+public interface RoomRepository extends CrudRepository<Room> {
+
+}
